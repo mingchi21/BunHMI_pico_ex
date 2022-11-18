058b3d397760b508bcb2274cbc3fbccf704634ba
mingchi <mingchi21@gmail.com>
2022-11-18 20:14:52 +0800

    update arduino code

--- arduino_pico/arduino_pico.ino
+++ arduino_pico/arduino_pico.ino
@@ -1,76 +1,116 @@
 /*
-  AnalogReadSerial
-
-  Reads an analog input on pin 0, prints the result to the Serial Monitor.
-  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogReadSerial
+  BunHMI Demo
 */
-
-#include <arduino-timer.h>
+#include <Servo.h>
+#include <arduino-timer.h> // https://www.arduino.cc/reference/en/libraries/arduino-timer/
+#define EOT 0x04 // Buntalk script termination
 
 auto timer = timer_create_default();
-int sec_tick_cnt = 0;
 
-int test_cnt = 0;
+Servo myservo;  // create servo object to control a servo
 
-bool sys_tick(void*){
-  char buff[128];
-  if(++sec_tick_cnt >= 1000){
+/**
+* timer tick call back
+*/
+bool timer_tick(void*) {
+  static int sec_tick_cnt = 0;
+  static int test_cnt = 0;
+  if (++sec_tick_cnt >= 1000) {
     // 1 Sec
     sec_tick_cnt = 0;
-    snprintf(buff, sizeof(buff),"w4.text(\"CNT:%d\")\n", test_cnt++);
-    Serial1.print(buff);
+    Serial1.printf("lab.text(\"Timer:%d\")\x04", test_cnt++);
   }
-  if((sec_tick_cnt % 100) == 0){
+  if ((sec_tick_cnt % 100) == 0) {
     // 100ms
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));  // turn the LED on (HIGH is the voltage level)  
-    
-    int sensorValue = (analogRead(A0)*100)/1024;
-    snprintf(buff, sizeof(buff),"w2.val(%d,1)\n", sensorValue);
-    Serial1.print(buff);
+    int sensorValue = (analogRead(A0) * 100) / 1024;
+    Serial1.printf("bar.val(%d,1)", sensorValue);    
+    Serial1.write(EOT);
   }
   return true;
 }
 
 // the setup routine runs once when you press reset:
 void setup() {
+  // Set  POW to PWM mode, reduce ripple for ADC
+  pinMode(23, OUTPUT);
+  digitalWrite(23, HIGH);
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-  // initialize serial communication at 9600 bits per second:
+  myservo.attach(D2);  // attaches the servo on pin 2 to the servo object
+  // initialize serial communication at 115200 bits per second:
   Serial1.setRX(PIN_SERIAL1_RX);
   Serial1.setTX(PIN_SERIAL1_TX);
-  Serial1.begin(115200);  
-  //Serial1.println("Hello Pico");
+  Serial1.begin(115200);
+  Serial1.setTimeout(10);
 
-  Serial.begin(9600); // USB serial
-
-  timer.every(1, sys_tick);
+  timer.every(1, timer_tick);
 }
 
+/**
+  * Description: Receive Hmi Data
+  * Parameters:
+  *  dat: Data buffer
+  *  dat_len: Data buffer length
+  * Return:
+  *  length of rx bytes
+*/
+int rxHmiData(char* dat, int dat_len) {
+  static char hmiBuff[256];
+  static uint8_t hmiBuffLen = 0;  
 
+  if (!Serial1.available()) {
+    return 0;
+  }
+  int rxlen = Serial1.readBytes(hmiBuff + hmiBuffLen, sizeof(hmiBuff) - hmiBuffLen);
+  int i;
+  hmiBuffLen += rxlen;
+  for (i = hmiBuffLen - 1; i >= 0; i--) {
+    if (hmiBuff[i] == EOT) {
+      // Got EOT Byte
+      hmiBuff[i++] = 0;  // Change EOT to NULL,  string  terminate
+      int hmi_len = (i < dat_len) ? i : dat_len;
+      // Copy hmiBuff to dat
+      memcpy(dat, hmiBuff, hmi_len);
+      // Move remain data to the head of hmiBuff
+      int remain_len = 0;
+      while (i < hmiBuffLen) {
+        hmiBuff[remain_len] = hmiBuff[i];
+        remain_len++;
+        i++;
+      }
+      hmiBuffLen = remain_len;
+      return hmi_len;
+    }
+  }
+  return 0;
+}
 
+const char* LED_ = "LED:";
+const char* SLI_ = "SLI:";
+void handleHmiData(const char* dat) {
+  int val;
+  if (strncmp(dat, LED_, strlen(LED_)) == 0) {
+    val = strtoul(dat + strlen(LED_), NULL, 0);
+    digitalWrite(LED_BUILTIN, val);
+    return;
+  }
+  if (strncmp(dat, SLI_, strlen(SLI_)) == 0) {
+    val = strtoul(dat + strlen(LED_), NULL, 0);    
+    int servo = map(val, 0, 100, 0, 180);     // scale it to use it with the servo (value between 0 and 180)    
+    myservo.write(servo);           
+    return;
+  }
+}
 // the loop routine runs over and over again forever:
 void loop() {
+  char rxbuff[256];
+  int rxlen;
+
   timer.tick();
-
-  if(Serial1.available()){
-    String str = Serial1.readString();
-    Serial.print(str);
+  // Check Data from HMI
+  rxlen = rxHmiData(rxbuff, sizeof(rxbuff));
+  if (rxlen) {
+    // Handle data from HMI
+    handleHmiData(rxbuff);
   }
-
-  // read the input on analog pin 0:
-  
-  // int sensorValue = analogRead(A0);
-  // // print out the value you read:
-  // digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  // Serial1.println(sensorValue);
-  // delay(100);                      // delay in between reads for stability    
-  // Serial.print("USB Ser=");
-  // Serial.println(sensorValue);
-  // digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
-  // delay(100);                      // wait for a second
 }
